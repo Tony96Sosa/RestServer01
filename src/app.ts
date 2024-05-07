ad6b323afa93dfeb011ab59ec673e2bcd144868c
Anthony Sosa Flores <kurt.sosa@soho.cl>
2024-05-07 13:43:02 -0300

    web server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log("Hola Mundo");
+import { envs } from "./config/envs";
+import { Server } from "./presentation/server";
+
+(async () => {
+  main();
+})();
+
+function main() {
+  console.log("Funcion main!");
+  const server = new Server({
+    port: envs.PORT,
+  });
+  server.start();
+}
